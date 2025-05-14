fa7da8c7f87669a95342fe74fc973ed49e22596b
alexhddev <21276888+alexhddev@users.noreply.github.com>
2025-05-14 11:14:00 +0300

    Tags

--- Starter/playwright.config.ts
+++ Starter/playwright.config.ts
@@ -2,6 +2,8 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
+  reporter: 'html',
+  grep: /Form/,
   webServer: {
     command: 'npm start',
     url: 'http://localhost:5000/',
