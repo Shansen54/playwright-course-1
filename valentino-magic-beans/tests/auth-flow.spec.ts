fdb23db39cf8c8e493d503200eca9fedef845909
alexhddev <21276888+alexhddev@users.noreply.github.com>
2025-07-10 14:56:13 +0300

    Sign up workflow

--- valentino-magic-beans/tests/auth-flow.spec.ts
+++ valentino-magic-beans/tests/auth-flow.spec.ts
@@ -1,7 +1,26 @@
 import { test } from '@playwright/test';
-import { createInbox } from './utils/EmailUtils'
+import { EmailUtils } from './utils/EmailUtils'
+import * as signUpPage from './pages/SignUp'
+import * as loginPage from './pages/Login'
 
-test('Generate inbox', async ()=>{
-    const inbox = await createInbox();
-    console.log(inbox)
+test('Sign up', async ({page})=>{
+    const emailUtils = new EmailUtils()
+    const inbox = await emailUtils.createInbox();
+
+    await page.goto('/signup')
+
+    await signUpPage.signUp(page, inbox.emailAddress)
+
+    const email = await emailUtils.waitForLatestEmail(inbox.id)
+    
+    // get the code\ from the email body:
+    const code = /([0-9]{6})$/.exec(email?.body!)![1];
+
+    await signUpPage.addConfirmationCode(page, code)
+
+    await loginPage.login(page, inbox.emailAddress, signUpPage.signUpData.pass)
+
+
+
+
 })
--- valentino-magic-beans/tests/utils/EmailUtils.ts
+++ valentino-magic-beans/tests/utils/EmailUtils.ts
@@ -1,9 +1,23 @@
 import { MailSlurp } from 'mailslurp-client';
 
+export class EmailUtils {
 
-export async function createInbox(){
-    const mailSlurp = new MailSlurp({ apiKey: process.env.MAIL_SLURP_API_KEY! })
-    const inbox = await mailSlurp.inboxController.createInboxWithDefaults();
-    return inbox;
+    private mailSlurp: MailSlurp;
+
+    constructor() {
+        this.mailSlurp = new MailSlurp({ apiKey: process.env.MAIL_SLURP_API_KEY! })
+    }
+
+    public async createInbox() {
+        const inbox = await this.mailSlurp.inboxController.createInboxWithDefaults();
+        return inbox;
+    }
+
+    public async waitForLatestEmail(inboxId: string) {
+        const email = await this.mailSlurp.waitForLatestEmail(inboxId, 30000)
+        return email;
+    }
 }
+
+
 
